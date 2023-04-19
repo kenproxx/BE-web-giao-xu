6377c3646dafa2f982d2c08df9b1123cb3388881
kiennt <kienhue98@gmail.com>
2023-04-19 20:43:12 +0700

    create new tag

--- src/main/java/com/example/airbnb/controller/TagController.java
+++ src/main/java/com/example/airbnb/controller/TagController.java
@@ -1,10 +1,14 @@
 package com.example.airbnb.controller;
 
+import com.example.airbnb.model.TagEntity;
 import com.example.airbnb.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -12,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TagController {
     @Autowired
     private TagService tagService;
+
+    @PostMapping("/add")
+    public ResponseEntity<TagEntity> createTag(@RequestBody TagEntity tagEntity) {
+        return tagService.createNewTag(tagEntity);
+    }
 }
--- src/main/java/com/example/airbnb/service/TagService.java
+++ src/main/java/com/example/airbnb/service/TagService.java
@@ -1,11 +1,15 @@
 package com.example.airbnb.service;
 
+import com.example.airbnb.model.TagEntity;
 import com.example.airbnb.repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 public class TagService {
@@ -13,5 +17,20 @@ public class TagService {
     @Autowired
     private TagRepository tagRepository;
 
+    public List<String> listKeyTag() {
+        LinkedList<String> listKeyTag = new LinkedList<>();
+        tagRepository.findAll().forEach(tag -> listKeyTag.add(tag.getKeyName()));
+        return listKeyTag;
+    }
 
+    public ResponseEntity<TagEntity> createNewTag(TagEntity tagEntity) {
+        List<String> listKeyTag = listKeyTag();
+        String keyName = tagEntity.getKeyName();
+        boolean isKeyTag = listKeyTag.stream().anyMatch(key -> key.equals(keyName));
+        if (isKeyTag) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        tagRepository.save(tagEntity);
+        return new ResponseEntity<>(tagEntity, HttpStatus.OK);
+    }
 }

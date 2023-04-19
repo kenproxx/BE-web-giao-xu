3e3f1ae455ea324761b065536b610f17c037d562
kiennt <kienhue98@gmail.com>
2023-04-19 20:17:58 +0700

    tao LogService

--- src/main/java/com/example/airbnb/repository/LogRepository.java
+++ src/main/java/com/example/airbnb/repository/LogRepository.java
@@ -2,6 +2,8 @@ package com.example.airbnb.repository;
 
 import com.example.airbnb.model.LogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LogRepository extends JpaRepository<LogEntity, Long> {
 }
---
+++ src/main/java/com/example/airbnb/service/LogService.java
@@ -0,0 +1,7 @@
+package com.example.airbnb.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogService {
+}
